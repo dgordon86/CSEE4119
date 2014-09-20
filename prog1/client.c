b6934cb2c962ef928182ecb564abce7af9bf25bd
Danny Gordon <dgordon@adobe.com>
2014-09-20 13:17:20 -0400

    client and server now using select to handle events

--- prog1/client.c
+++ prog1/client.c
@@ -8,6 +8,8 @@
 #include <netdb.h> 
 #include <arpa/inet.h>
 
+fd_set read_fds, write_fds; //file descriptor list contains stdin and socket connection to server
+
 void error(const char *msg)
 {
     perror(msg);
@@ -16,45 +18,107 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n;
-    //char *in_serv_addr;
+    struct timeval tv;
+    int sockfd, portnum, nbytes, ipnum, i;
+    int fdmax;          //max file descriptor
+    char *ipstring;
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
     char buffer[256];
     if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       fprintf(stderr,"usage %s ipaddress portnum\n", argv[0]);
        exit(0);
     }
-    portno = atoi(argv[2]);
+    ipstring = argv[1];
+    portnum = atoi(argv[2]);
+   
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    ipnum = inet_addr(ipstring);
+    if(ipnum < 0) {
+        error("ERROR ip address appears invalid");
+    }
+    //set up server address
+    serv_addr.sin_addr.s_addr = ipnum;
+    inet_pton(AF_INET, argv[1], &(serv_addr.sin_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(portnum);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
         error("ERROR opening socket");
     }
-
-   bzero((char *) &serv_addr, sizeof(serv_addr));
-    inet_pton(AF_INET, argv[1], &(serv_addr.sin_addr));
     
-    serv_addr.sin_family = AF_INET;
-    /*
-    bcopy((char *)server->h_addr,
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_addr.s_addr = inet_addr(in_serv_addr);*/
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
         error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
+    tv.tv_sec = 5;
+    tv.tv_usec = 0;
+    
+   /* FD_ZERO(&read_fds);    //clear file descriptors
+    FD_SET(sockfd, &read_fds);    //add server socket
+    FD_SET(STDIN_FILENO, &read_fds);
+    fdmax = (sockfd > STDIN_FILENO) ? sockfd : STDIN_FILENO;
+    */
+    int serverUp = 1;
+    while(serverUp) {
+        FD_ZERO(&read_fds); //clear file descriptors
+        FD_ZERO(&write_fds);
+        FD_SET(sockfd, &read_fds);    //add server socket
+        FD_SET(STDIN_FILENO, &read_fds);
+        FD_SET(sockfd, &write_fds);
+        
+        if (select(sockfd+1, &read_fds, &write_fds, NULL, NULL) == -1) {
+            perror("select");
+            exit(4);
+        }
+       
+        printf("Command:\n");
+        //check if data from Server
+        if(FD_ISSET(sockfd, &read_fds)) {
+            // handle data server
+            if ((nbytes = recv(sockfd, buffer, sizeof buffer, 0)) <= 0) {
+                // got error or connection closed by client
+                if (nbytes == 0) {
+                    // connection closed
+                    printf("Server hung up: exiting\n");
+                } else {
+                    error("recv");
+                }
+                //close connection and prepare to exit
+                serverUp = 0;
+                close(sockfd);
+                FD_CLR(sockfd, &read_fds);
+            }
+            else{
+                printf("%s", buffer);
+                memset(&buffer, 0, sizeof(buffer));//reset buffer to zero
+            }
+        }
+        else if(FD_ISSET(STDIN_FILENO, &read_fds)) {
+            if (fgets(buffer,sizeof(buffer), stdin)) {
+                //printf("Length: %lu\n" ,strlen(buffer));
+                if(FD_ISSET(sockfd, &write_fds)) {
+                    if (send(sockfd, buffer, strlen(buffer), 0) == -1) {
+                        error("send");
+                    }
+                }
+                memset(&buffer, 0, sizeof(buffer));
+            }
+        }
+       /* printf("Please enter the message: ");
+        bzero(buffer,256);
+        fgets(buffer,255,stdin);
+        n = write(sockfd,buffer,strlen(buffer));
+        if (n < 0)
+            error("ERROR writing to socket");
+    
+        bzero(buffer,256);
+        n = read(sockfd,buffer,255);
+        if (n < 0)
+            error("ERROR reading from socket");
+        printf("%s\n",buffer);*/
+        
+    }
     close(sockfd);
+   
     return 0;
 }
--- prog1/server.c
+++ prog1/server.c
@@ -1,55 +1,162 @@
-/* A simple server in the internet domain using TCP
-   The port number is passed as an argument */
+/*
+ * server.c
+ * Server implementation for Programming Assignment 1: 4119
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+
+fd_set master;    // master file descriptor list
+fd_set read_fds;  // temp file descriptor list for select()
+
+void cleanExit() {
+    exit(0);
+}
 
 void error(const char *msg)
 {
     perror(msg);
     exit(1);
 }
+/*/ get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+    
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}*/
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno;
-     socklen_t clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
+    int portnum;      // portnum
+   
+    int fdmax;        // maximum file descriptor number
     
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(PF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
+    int listener;     // listening socket descriptor
+    int newfd;        // newly accept()ed socket descriptor
+    struct sockaddr_in serv_addr, cli_addr; // client address
+    socklen_t addrlen;
+    
+    char buf[256];      // buffer for client data
+    char relayMsg[300]; //buffer for message not sure if we need this
+    int i,j;
+    int nbytes;         //bytes sent
+    
+    //Get port number from command line
+    if (argc < 2) {
+        fprintf(stderr,"ERROR, no port provided\n");
+        exit(1);
+    }
+    portnum = atoi(argv[1]);
+    
+    FD_ZERO(&master);    //clear file descriptors
+    FD_ZERO(&read_fds);
+    
+    //set up server address
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portnum);
+    
+    //get socket
+    listener = socket(PF_INET, SOCK_STREAM, 0);
+    if (listener < 0)
         error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, 
-                 (struct sockaddr *) &cli_addr, 
-                 &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     close(newsockfd);
-     close(sockfd);
-     return 0; 
+    //reuse port num yes
+    int yes = 1;
+    setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+    
+    // bind to socket
+    if (bind(listener, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        error("ERROR on binding");
+    }
+    
+    
+    if (listen(listener, 10) == -1) {
+        perror("listen");
+        exit(3);
+    }
+    
+    // add the listener to the master set
+    FD_SET(listener, &master);
+    
+    // keep track of the biggest file descriptor
+    fdmax = listener; // so far, it's this one
+    
+    //server loop
+    while(1) {
+        read_fds = master; // copy it
+        if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1) {
+            perror("select");
+            exit(4);
+        }
+        
+        // run through the existing connections looking for data to read
+        for(i = 0; i <= fdmax; i++) {
+            if (FD_ISSET(i, &read_fds)) { //
+                if (i == listener) {
+                    // handle new connections
+                    addrlen = sizeof cli_addr;
+                    newfd = accept(listener,
+                                   (struct sockaddr *)&cli_addr,
+                                   &addrlen);
+                    
+                    if (newfd == -1) {
+                        perror("accept");
+                    } else {
+                        FD_SET(newfd, &master); // add to master set
+                        if (newfd > fdmax) {    // keep track of the max
+                            fdmax = newfd;
+                        }
+                        printf("selectserver: new connection from s on "
+                               "socket %d\n",newfd);
+                    }
+                } else {
+                    // handle data from a client
+                    if ((nbytes = recv(i, buf, sizeof buf, 0)) <= 0) {
+                        // got error or connection closed by client
+                        if (nbytes == 0) {
+                            // connection closed
+                            printf("selectserver: socket %d hung up\n", i);
+                        } else {
+                            perror("recv");
+                        }
+                        close(i); // bye!
+                        FD_CLR(i, &master); // remove from master set
+                    } else {
+                        //reset relay message to null
+                        memset(relayMsg, 0, 300);
+                        sprintf(relayMsg, "%d: ", i);
+                        strncat(relayMsg, buf, nbytes);
+                        printf("%s", relayMsg);
+                        
+                        // we got some data from a client
+                        for(j = 0; j <= fdmax; j++) {
+                            // send to everyone!
+                            if (FD_ISSET(j, &master)) {
+                                // except the listener and ourselves
+                                if (j != listener && j != i) {
+                                    if (send(j, relayMsg, strlen(relayMsg), 0) == -1) {
+                                        perror("send");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } // END handle data from client
+            } // END got new incoming connection
+        } // END looping through file descriptors
+    } // END while
+    
+    return 0;
 }
